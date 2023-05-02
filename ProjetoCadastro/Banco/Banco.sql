b5b8cb00c3fb9f99a94bca7ef4da64a6c6404a68
Larissa-Alves-de-Oliveira <124682645+Larissa-Alves-de-Oliveira@users.noreply.github.com>
2023-05-02 11:10:34 -0300

    Add files via upload

---
+++ ProjetoCadastro/Banco/Banco.sql
@@ -0,0 +1,13 @@
+--Arquivo de criação do banco de dados
+
+DROP DATABASE IF EXISTS projeto1;
+CREATE DATABASE projeto1;
+USE projeto1;
+
+CREATE TABLE usuario(
+	id_usuario int(4) primary key auto_increment,
+    nome_usuario varchar(50) not null,
+    email_usuario varchar(50) not null,
+    fone_usuario varchar(30) null,
+    senha varchar(30) not null
+);
