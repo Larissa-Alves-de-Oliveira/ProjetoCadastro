d6950b1acba4ca97db04e18a24e5c06a3598aed1
Larissa-Alves-de-Oliveira <alvesoliveira977@gmail.com>
2023-05-09 09:20:58 -0300

    b

--- ProjetoCadastro/conexao.php
+++ ProjetoCadastro/conexao.php
@@ -5,7 +5,7 @@
     $username = "root";
     $password = "";
     $database = "projeto1";
-    $port = 3306;
+    $port = 3307;
 
     $con = mysqli_connect($hostname, $username, $password, $database, $port);
 
