0452a0b715f4179b34aa2df4399b05bab46bef1b
Larissa-Alves-de-Oliveira <alvesoliveira977@gmail.com>
2023-05-16 11:21:27 -0300

    delete

---
+++ ProjetoCadastro/excluir_usuario_exe.php
@@ -0,0 +1,13 @@
+<?php
+    include('conexao.php');
+    $id = $_GET['id_usuario'];
+
+    $sql = "DELETE from usuario where id_usuario ='$id'"; 
+            //Comando que funciona apenas no banco de dados
+    echo $sql;
+    $result = mysqli_query($con,$sql); //Envia as informações para o banco de dados
+    if($result)
+        echo "Dados excluídos com sucesso!<br>";
+    else
+        echo "Erro ao excluir dados: $msqli_erro($con)"
+?>
--- ProjetoCadastro/listar_usuarios.php
+++ ProjetoCadastro/listar_usuarios.php
@@ -24,6 +24,7 @@
             <th>Email</th>
             <th>Telefone</th>
             <th>Altera</th>
+            <th>Deleta</th>
         </tr>
         <?php
             do{
@@ -33,6 +34,7 @@
                 echo "<td>".$row['email_usuario']."</td>";
                 echo "<td>".$row['fone_usuario']."</td>";
                 echo "<td><a href='altera.php?id_usuario=" .$row['id_usuario']. "'>Alterar</a></td>";
+                echo "<td><a href='excluir_usuario_exe.php?id_usuario=" .$row['id_usuario']. "'>Deletar</a></td>";
                 echo "</tr>";
             }while($row =mysqli_fetch_array($result));
         ?>
